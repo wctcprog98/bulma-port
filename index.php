40a939c393a484fa888ad65366331a2b9c3e215d
Nenye <chinenyediei@gmail.com>
2020-11-28 00:52:02 -0600

    trying to add php page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <p>SEND EMAIL</p>
+        <form class="contact-form" action="contact.php" method="post">
+            <input type="text" name="name" placeholder="full name">
+            <input type="text" name="email" placeholder="Your e-mail">
+            <input type="text" name="aubject" plaecholder="subject">
+            <textarea name="message" placeholder="Message"></textarea>
+        </form>
+    </main>
+</body>
+</html>
